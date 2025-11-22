f25baaeac45b2d480700f6e82842d8159f56cb88
cocktailpeanut <cocktailpeanuts@proton.me>
2025-11-21 21:23:23 -0500

    init

---
+++ pinokio.js
@@ -0,0 +1,44 @@
+module.exports = {
+  title: "AMP",
+  icon: "icon.webp",
+  description: "Amp is a frontier coding agent for your terminal and editor, built by Sourcegraph",
+  link: "https://ampcode.com/manual",
+  install: [{
+    method: "shell.run",
+    params: {
+      message: "npm install -g @sourcegraph/amp@latest"
+    }
+  }],
+  uninstall: [{
+    method: "shell.run",
+    params: {
+      message: "npm uninstall -g @sourcegraph"
+    }
+  }],
+  update: [{
+    method: "shell.run",
+    params: {
+      message: "npm install -g @sourcegraph/amp@latest"
+    }
+  }],
+  run: [{
+    when: "{{platform === 'win32'}}",
+    method: "shell.run",
+    params: {
+      shell: "{{kernel.path('bin/miniconda/Library/bin/bash.exe')}}",
+      message: "amp",
+      path: "{{args.cwd}}",
+      input: true
+    }
+  }, {
+    when: "{{platform !== 'win32'}}",
+    id: "run",
+    method: "shell.run",
+    params: {
+      shell: "{{kernel.path('bin/miniconda/Library/bin/bash.exe')}}",
+      message: "amp",
+      path: "{{args.cwd}}",
+      input: true
+    }
+  }]
+}
